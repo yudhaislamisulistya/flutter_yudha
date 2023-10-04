6b0d745623e66039d3111d55baf6cd5ea7c8b4bd
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2023-10-04 21:26:21 +0700

    update rest api

--- 20_rest_api/rest_api/lib/main.dart
+++ 20_rest_api/rest_api/lib/main.dart
@@ -1,9 +1,13 @@
+// ignore_for_file: avoid_print, use_build_context_synchronously
+
 import 'dart:convert';
 
+import 'package:cherry_toast/cherry_toast.dart';
+import 'package:cherry_toast/resources/arrays.dart';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:rest_api/models/book_model.dart';
-import 'package:motion_toast/motion_toast.dart';
+import 'package:rest_api/utils/fetch.dart';
 
 void main() {
   runApp(const MyApp());
@@ -142,7 +146,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   TextButton(
                     onPressed: () {
-                      Navigator.pop(context);
                       var data = {
                         "book_id": books.length + 1,
                         "title": titleController.text,
@@ -150,13 +153,34 @@ class _MyHomePageState extends State<MyHomePage> {
                       };
                       Dio().post("https://bookapi-1-v1905306.deta.app/api/v1/books/", data: jsonEncode(data)).then((response) {
                         var response_detail = response.data["detail"];
+                        Navigator.pop(context);
                         if (response_detail["status_code"] == 201) {
-                          print("Data Berhasil Ditambahkan");
+                          CherryToast(
+                            icon: Icons.alarm_add,
+                            themeColor: Colors.amber,
+                            title: const Text(""),
+                            displayTitle: false,
+                            description: const Text("Data Berhasil Ditambahkan", style: TextStyle(color: Colors.black)),
+                            toastPosition: Position.bottom,
+                            animationDuration: const Duration(milliseconds: 1000),
+                            autoDismiss: true,
+                          ).show(context);
                           books.add(BookModel.fromJson(response.data["data"]));
                           setState(() {});
+                          // before convert
+                          // json
+                          // "id": 1
+                          // "name": "Hadi"
+
+                          // after convert
+                          // object
+                          // id: 1
+                          // name: "Hadi"
                         } else {
                           print("Data Gagal Ditambahkan");
                         }
+                        titleController.clear();
+                        yearController.clear();
                       });
                     },
                     child: const Text("Save"),
@@ -166,8 +190,8 @@ class _MyHomePageState extends State<MyHomePage> {
             },
           );
         },
-        child: const Icon(Icons.add),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        child: const Icon(Icons.add),
       ),
       body: SingleChildScrollView(
         child: ListView.builder(
@@ -178,6 +202,130 @@ class _MyHomePageState extends State<MyHomePage> {
             return ListTile(
               title: Text(books[index].title),
               subtitle: Text(books[index].year.toString()),
+              trailing: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      print("Icon Delete Berhasil Ditekan");
+
+                      // alert confirmation deletet
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: const Text("Delete Book"),
+                            content: const Text("Are you sure want to delete this book?"),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text("Cancel"),
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  var response = await deleteData(books[index].bookId);
+                                  var responseDetail = response["detail"];
+                                  if (responseDetail["status_code"] == 200) {
+                                    CherryToast(
+                                      icon: Icons.check,
+                                      themeColor: Colors.amber,
+                                      title: const Text(""),
+                                      displayTitle: false,
+                                      description: const Text("Data Berhasil Dihapus", style: TextStyle(color: Colors.black)),
+                                      toastPosition: Position.bottom,
+                                      animationDuration: const Duration(milliseconds: 1000),
+                                      autoDismiss: true,
+                                    ).show(context);
+                                    books.removeAt(index);
+                                    setState(() {});
+                                  } else {
+                                    print("Data Gagal Dihapus");
+                                  }
+                                  Navigator.pop(context);
+                                },
+                                child: const Text("Delete"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: const Icon(Icons.delete),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) {
+                          titleController.text = books[index].title;
+                          yearController.text = books[index].year.toString();
+                          return AlertDialog(
+                            title: const Text("Edit Book"),
+                            content: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                TextField(
+                                  controller: titleController,
+                                  decoration: const InputDecoration(
+                                    labelText: "Title",
+                                  ),
+                                ),
+                                TextField(
+                                  controller: yearController,
+                                  decoration: const InputDecoration(
+                                    labelText: "Year",
+                                  ),
+                                ),
+                              ],
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: const Text("Cancel"),
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  var data = {
+                                    "book_id": books[index].bookId,
+                                    "title": titleController.text,
+                                    "year": int.parse(yearController.text),
+                                  };
+                                  var response = await updateData(books[index].bookId, data);
+                                  var responseDetail = response["detail"];
+                                  if (responseDetail["status_code"] == 200) {
+                                    CherryToast(
+                                      icon: Icons.check,
+                                      themeColor: Colors.amber,
+                                      title: const Text(""),
+                                      displayTitle: false,
+                                      description: const Text("Data Berhasil Diubah", style: TextStyle(color: Colors.black)),
+                                      toastPosition: Position.bottom,
+                                      animationDuration: const Duration(milliseconds: 1000),
+                                      autoDismiss: true,
+                                    ).show(context);
+                                    books[index] = BookModel.fromJson(data);
+                                    setState(() {});
+                                  } else {
+                                    print("Data Gagal Diubah");
+                                  }
+                                  Navigator.pop(context);
+                                  titleController.clear();
+                                  yearController.clear();
+                                },
+                                child: const Text("Update"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    child: const Icon(Icons.edit),
+                  )
+                ],
+              ),
             );
           },
         ),
